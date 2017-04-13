426cbe8cdf32c77f635d05d18c7372df7a17c516
GiovanePimentel <giovane.rcgp@gmail.com>
2017-04-13 00:39:57 -0300

    Crud completo.
    Crud completo.
    Observar que existe erro de chamada de CSS e JS no header/footer.

---
+++ customers/add.php
@@ -0,0 +1,42 @@
+
+  <div class="row">
+    <div class="form-group col-md-3">
+      <label for="campo1">Município</label>
+      <input type="text" class="form-control" name="customer['city']">
+    </div>
+    
+    <div class="form-group col-md-2">
+      <label for="campo2">Telefone</label>
+      <input type="text" class="form-control" name="customer['phone']">
+    </div>
+    
+    <div class="form-group col-md-2">
+      <label for="campo3">Celular</label>
+      <input type="text" class="form-control" name="customer['mobile']">
+    </div>
+    
+    <div class="form-group col-md-1">
+      <label for="campo3">UF</label>
+      <input type="text" class="form-control" name="customer['state']">
+    </div>
+    
+    <div class="form-group col-md-2">
+      <label for="campo3">Inscrição Estadual</label>
+      <input type="text" class="form-control" name="customer['ie']">
+    </div>
+    
+    <div class="form-group col-md-2">
+      <label for="campo3">UF</label>
+      <input type="text" class="form-control">
+    </div>
+  </div>
+  
+  <div id="actions" class="row">
+    <div class="col-md-12">
+      <button type="submit" class="btn btn-primary">Salvar</button>
+      <a href="index.php" class="btn btn-default">Cancelar</a>
+    </div>
+  </div>
+</form>
+
+<?php include(FOOTER_TEMPLATE); ?>
